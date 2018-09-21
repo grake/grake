93b00f9a236ef6af888ff8370379a35d089f9a2d
grake <43462684+grake@users.noreply.github.com>
2018-09-21 14:52:37 +0900

    뺅랙

---
+++ 뺅랡/Assets/shootpos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootpos : MonoBehaviour {
+
+    public Transform Shootpos;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Orbit();
+    }
+
+    void Orbit()
+    {
+        if (Shootpos.transform.position.y >= -0.073f)
+        {
+            Shootpos.transform.Translate(Vector3.down);
+        }
+        if(Shootpos.transform.position.y <= -0.073f)
+        {
+            Shootpos.transform.Translate(Vector3.left);
+        }
+        if (Shootpos.transform.position.y <= 0.073f)
+        {
+            Shootpos.transform.Translate(Vector3.right);
+        }
+        if(Shootpos.transform.position.x <= -0.07f)
+        {
+            Shootpos.transform.Translate(Vector3.up);
+        }
+    }
+}
