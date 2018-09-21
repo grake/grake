a6264bc9686aadd914ef0de69c62f391392eaa4a
grake <43462684+grake@users.noreply.github.com>
2018-09-21 14:49:28 +0900

    유니티

---
+++ Scripts/Char_Contorller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_Contorller : MonoBehaviour {
+
+    private float TimeLeft = 0.5f;
+    private float NextTime = 0.0f;
+    public float Speed;
+    int bullet_num = 5;
+    public GameObject Shoot;
+    public Transform shootpos;
+    public GameObject cha;
+    Vector3 movement;
+
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        if (cha.transform.position.x < -10f || cha.transform.position.x > 9.5f)
+            Destroy(cha);
+        if (cha.transform.position.y < -6f || cha.transform.position.y > 5f)
+            Destroy(cha);
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * Speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * Speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Speed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(Shoot).transform.position = shootpos.position;
+        }
+        Autoshoot();
+    }
+    void Autoshoot()
+    {
+        if(Time.time > NextTime)
+        {
+            NextTime = TimeLeft + Time.time;
+                Debug.Log(bullet_num);
+                Instantiate(Shoot).transform.position = shootpos.position;
+                //if(bullet_num == 1)
+                //{
+                 //   Debug.Log(bullet_num);
+                //}
+                //bullet_num += -1;
+        }
+    }
+}
---
+++ Scripts/otherChar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class otherChar : MonoBehaviour {
+
+    private float TimeLeft = 1.0f;
+    private float NextTime = 0.0f;
+    public float Speed;
+    int bullet_num = 5;
+    public GameObject Shoot;
+    public Transform shootpos;
+    public GameObject cha;
+    Vector3 movement;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cha.transform.position.x < -10f || cha.transform.position.x > 9.5f)
+            Destroy(cha);
+        if (cha.transform.position.y < -6f || cha.transform.position.y > 5f)
+            Destroy(cha);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * Speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * Speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Speed);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            Instantiate(Shoot).transform.position = shootpos.position;
+        }
+    }
+}
